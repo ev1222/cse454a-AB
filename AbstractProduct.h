e571b161fea971b6900eeb7a22a537710dbaa091
ev1222 <evan.trabitz@wustl.edu>
2022-10-21 14:06:08 -0500

    Created first few headers
    Created headers for AbstractProduct, Supplier, and general utils file

---
+++ AbstractProduct.h
@@ -0,0 +1,8 @@
+#include <utils.h>
+
+class AbstractProduct {
+public:
+    virtual ~AbstractProduct() = default;
+private:
+
+};
---
+++ utils.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+//TODO: don't use this
+using namespace std;
