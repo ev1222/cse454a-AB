52ea9dbc1ce883d34612d16ed9616dc111848b2f
ev1222 <evan.trabitz@wustl.edu>
2022-11-03 10:21:00 -0500

    changing base to python

--- AbstractProduct.h
+++
@@ -1,8 +0,0 @@
-#include <utils.h>
-
-class AbstractProduct {
-public:
-    virtual ~AbstractProduct() = default;
-private:
-
-};
--- utils.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
-
-//TODO: don't use this
-using namespace std;
