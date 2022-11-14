030b1d3f61a420a56273f8353f29253dbddac35f
Maple-Canoe <59814536+Maple-Canoe@users.noreply.github.com>
2022-11-14 17:56:35 -0600

    data processing

--- DataProcessor.py
+++ DataProcessor.py
@@ -1,5 +1,14 @@
 # data shapes
-# supplier: name, products, product cost, capacity
-# customer: name, products, demand
-# lane: name, customer, supplier, transport cost 
+# Product: name, supplier, cost
+#  -> products(dictionary): [key=product_name, value=list of Product]
+# Lane: customer, supplier, transport cost, product
+#  -> lanes(list): list of Lane
+# Customer: name, product_demand(list of tuples)
+#  -> customers_dict(dictionary): [key=customer_name, value=list of (product_name, demand)]
+#  -> customers(list): list of Customer
+# supplier: name, product_capacity(list of tuples)
+#  -> suppliers_dict(dictionary): [key=supplier_name, value=list of (Product, capacity)]
+#  -> suppliers(list): list of Supplier
+
+
 
