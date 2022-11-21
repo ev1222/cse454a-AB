adc567390e67afdf2670d46f8cb0eb98c61d2b74
Kon Yoo <konyoo@wustl.edu>
2022-11-21 10:15:47 -0600

    round robin pseudocode

---
+++ Roundrobin.py
@@ -0,0 +1,45 @@
+###### Pseudocode
+
+# Given a list of lanes, sort by cheapest supplier production cost,
+# and take percentage of that capacity for each customer round-robin style,
+# removing old lane from list and adding a new lane with x% less capacity.
+
+# Take a brewery 20% of demand
+
+# Optimize by production cost
+
+customer:brewery
+
+optimize(lanes, percentage):
+	sorted_lanes = sort lanes by cheapest supplier production cost
+	sc = SupplyChain()
+
+		sorted_customer = [customer : percentage] # could use PQ
+		initial_demand = [customer: demand]
+		for each supplier:
+			for each customer:
+					percentage = customer.demand / supplier.capacity
+					map customer and percentage to sorted_customer
+		``
+	while sorted_lanes is not empty:
+		lane = lane popped from sorted_lanes		
+
+		for each customer in customers:
+			curr_customer = sorted_custmor.pop(customer)
+			ingredient_used = percentage * initial_demand[curr_customer]
+			remainder = max(capacity_remainder or curr_customer.demand)			
+			lane.capacity -= ingredient_used or remainder;
+			curr_customer.demand -= ingredient_used or remainder;
+			sorted_customers.add(curr_customer)
+			
+			if curr_customer.demand < 0: continue
+
+			if lane.capacity < 0:
+					Remove lane from sorted_lanes
+					Add lane to sc
+					break
+
+		return sc				
+			
+			
+			
